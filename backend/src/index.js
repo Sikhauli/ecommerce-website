c12d21e404a797547b1743b64db25fa209eb25d1
sikhauli <sikhaulivl@gmail.com>
2024-07-18 11:09:54 +0200

    fix backend error

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,9 +14,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const clientWebsite = process.env.CLIENT_WEBSITE || "http://localhost:5173" || "https://ecommerce-website-vv87.onrender.com";
-const adminWebsite = process.env.ADMIN_WEBSITE || "http://localhost:5173" || "https://ecommerce-website-vv87.onrender.com" ;
-const testWebsite = "http://localhost:4173" || "https://ecommerce-website-vv87.onrender.com";
+const clientWebsite =  "https://ecommerce-website-vv87.onrender.com";
+const adminWebsite =  "https://ecommerce-website-vv87.onrender.com";
 
 //connect to DB
 //listen after db connection has been est
@@ -35,7 +34,7 @@ app.use(cookieParser());
 app.use("/api/uploads", express.static("./src/uploads")); //for accessing images
 app.use(
   cors({
-    origin: [clientWebsite, adminWebsite, testWebsite],
+    origin: [clientWebsite, adminWebsite],
     credentials: true,
   })
 );
